cdb358b270cb3fb1106e97cc5379420d0ec53d0b
RifqiM <rifqi546@gmail.com>
2022-08-05 19:00:42 +0700

    Update dashboard_test.py

--- dashboard_test.py
+++ dashboard_test.py
@@ -12,7 +12,7 @@ st.write("Saya merasa heran karena selama ini Hollywood menilai atau menyiarkan 
 "Bagaimana dengan Profitabilitas, atau % dari anggaran yang dipulihkan? "
 "Terutama di Amerika, di mana setiap film memiliki biaya marketing tidak yang kalah tinggi dengan biaya produksi."
 " Jika kita menggunakan profitabilitas sebagai indeks, hasil yang ditunjukkan sangat berbeda." 
-"Contoh pada 2007, misalnya, di mana film terlaris pada tahun tersebut adalah Pirates Of The Caribbean: At Worlds End." )
+" Contoh pada 2007, misalnya, di mana film terlaris pada tahun tersebut adalah Pirates Of The Caribbean: At Worlds End." )
 image = Image.open("imgs/poc_awe.jpg")
 st.image(image)
 st.write("Dengan keuntungan sebesar 9.6 milliar dollar yang ternyata hanya memulihkan 320% dari budget sebesar 300 juta dollar." 
